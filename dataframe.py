39a0983a9de525b9a284a56d84d8fb622977b39c
airwick989 <73313597+airwick989@users.noreply.github.com>
2022-02-25 17:22:20 -0500

    Reformatted csv data, composed dataframe from csv, tested foursquare API call with latitude and longtitude

---
+++ dataframe.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import json
+
+df = pd.read_csv (r"Automated Speed Enforcement Locations.csv")
+
+latitudes = []
+longtitudes = []
+
+i= 0
+for coordinate in df["geometry"]:
+    placeholder = json.loads(coordinate)
+    coordinate = placeholder["coordinates"]
+    df["geometry"][i] = coordinate
+    latitudes.append(coordinate[1])
+    longtitudes.append(coordinate[0])
+    i = i+1
+
+df["latitude"] = latitudes
+df["longtitude"] = longtitudes
+
--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://api.foursquare.com/v3/places/search?query=coffee&near=toronto&limit=10"
+url = "https://api.foursquare.com/v3/places/search?ll=43.72%2C-79.57&radius=100000&categories=12057&limit=50"
 
 headers = {
     "Accept": "application/json",
