81ed305c5ef47e2e81880fd5f0d6efcb5bc32484
airwick989 <73313597+airwick989@users.noreply.github.com>
2022-04-03 19:30:20 -0400

    send help

--- fqapi_to_csv.py
+++ fqapi_to_csv.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pandas as pd
 import json
 from shapely.geometry import shape, Point
@@ -27,33 +26,3 @@ for p in points_with_hood_id:
     bicycle_shop_coordinates_du.write(f"{p['lat']},{p['lon']},{p['hood_id']}")
     bicycle_shop_coordinates_du.write("\n")
 
-=======
-import pandas as pd
-import json
-from shapely.geometry import shape, Point
-
-bicycle_shop_coordinates_du = open("bicycle_shop_coordinates.csv", "w")
-
-points_with_hood_id = [] 
-
-data = json.load(open("FoursquareData.json", "r"))
-for i in data["results"]:
-    lat = i["geocodes"]["main"]["latitude"]
-    lon = i["geocodes"]["main"]["longitude"]
-    points_with_hood_id.append( { "point": Point(lon, lat), "hood_id": None, "lon": lon, "lat": lat } )
-
-with open("Neighbourhoods.json", "r") as f:
-    geojs = json.load(f)
-
-for feature in geojs["features"]:
-    polygon = shape(feature["geometry"])
-    for point in points_with_hood_id:
-        print(point["point"])
-        if polygon.contains(point["point"]):
-            point["hood_id"] = feature["properties"]["AREA_SHORT_CODE"]
-
-for p in points_with_hood_id:
-    bicycle_shop_coordinates_du.write(f"{p['lat']},{p['lon']},{p['hood_id']}")
-    bicycle_shop_coordinates_du.write("\n")
-
->>>>>>> 2285bddec2c45d4c0465bd03ce91cc7006cd5fa7
