3af547a309528813212b74c863ca81e2cb62d294
airwick989 <73313597+airwick989@users.noreply.github.com>
2022-04-03 21:37:59 -0400

    began constructing new dataframe for heatmap

--- dataframe.py
+++ dataframe.py
@@ -1,24 +1,32 @@
+from re import I
 import pandas as pd
 import json
 
-df = pd.read_csv (r"bicycle_thefts.csv")
+df = pd.read_csv (r"bicycle_shop_coordinates.csv")
 
-latitudes = []
-longtitudes = []
+predata = {
+    'hood_id': [],
+    'number of bike shops': []
+}
 
-i= 0
-for coordinate in df["geometry"]:
-    placeholder = json.loads(coordinate)
-    coordinate = placeholder["coordinates"]
-    df["geometry"][i] = coordinate
-    latitudes.append(coordinate[1])
-    longtitudes.append(coordinate[0])
-    i = i+1
+hoodlist = {}
+for hood in df['hood_id']:
+    if hood not in hoodlist:
+        hoodlist[hood] = 1
+    else:
+        hoodlist[hood] += 1
 
-df["latitude"] = latitudes
-df["longtitude"] = longtitudes
 
-# del df['_id']
-# del df['Location_Code']
+df = df.drop_duplicates(subset='hood_id')
+df.reset_index(drop=True, inplace=True)
 
-coordinates = zip(latitudes, longtitudes)
+for key, value in hoodlist.items():
+  predata['hood_id'].append(key)
+  predata['number of bike shops'].append(value)
+
+df2 = pd.DataFrame(predata)
+
+df2['latitude'] = df['latitude']
+df2['longtitude'] = df['longtitude']
+
+print(df2)  #Now add number of bike thefts per hood_id, could also add name for each hood_id
